c725a3936ed76980f1a362e92caebb457ec2e5d5
Aman Chauhan <amangotpro@gmail.com>
2022-07-17 17:14:38 +0530

    refactor: separation

--- main.js
+++ main.js
@@ -6,26 +6,26 @@ let displayColor = document.getElementsByClassName("colorDisplay");
 
 const numberOfColors = 6;
 
-button.addEventListener("click", () => {
-    let currentMode = modes.options[modes.selectedIndex].value
 
+getColorData()
+
+button.addEventListener("click", () => {
+    getColorData()
+})
+
+function getColorData(){
+    let currentMode = modes.options[modes.selectedIndex].value
     let seedColor = document.getElementById("seedColor").value
     let currentSeedColor = seedColor.slice(1, seedColor.length)
 
-    displayColor[0].style.backgroundColor = currentSeedColor;
-    codes[0].textContent = seedColor;
-
-
     fetch(`https://www.thecolorapi.com/scheme?hex=${currentSeedColor}&mode=${currentMode}&count=${numberOfColors}`)
-
 
     .then(res => res.json())
     .then(data => {
-            for (let i=1; i<numberOfColors; i++){
+            for (let i=0; i<numberOfColors; i++){
                 codes[i].textContent = data.colors[i].hex.value;
-                displayColor[i].style.backgroundColor = data.colors[i].hex.value
+                displayColor[i].style.backgroundColor = codes[i].textContent
             }
         }
         );
-
-})
+}
