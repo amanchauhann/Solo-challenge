7361875f49437080c0e3cd10ae6feee8f10a0d3e
Aman Chauhan <amangotpro@gmail.com>
2022-07-17 16:12:30 +0530

    refactoring

--- main.js
+++ main.js
@@ -4,24 +4,14 @@ let codes = document.getElementsByClassName("colorCode");
 let displayColor = document.getElementsByClassName("colorDisplay");
 
 
-// displayColor[0].style.backgroundColor = "#F55A5A";
-// document.getElementById("p2").style.color
-// let currentSeedColor = document.getElementById("seedColor").value 
-
-// currentSeedColorr = currentSeedColor.slice(1, currentSeedColor.length)
-
-
 const numberOfColors = 6;
 
 button.addEventListener("click", () => {
     let currentMode = modes.options[modes.selectedIndex].value
-    console.log(currentMode)
-
-    // codes[0].textContent = "aman"
 
     let seedColor = document.getElementById("seedColor").value
     let currentSeedColor = seedColor.slice(1, seedColor.length)
-    console.log(currentSeedColor)
+
     displayColor[0].style.backgroundColor = currentSeedColor;
     codes[0].textContent = seedColor;
 
@@ -39,5 +29,3 @@ button.addEventListener("click", () => {
         );
 
 })
-
-// console.log((data).colors[0].hex.value)
